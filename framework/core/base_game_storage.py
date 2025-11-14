b985848b84823b58c818c70268715c363968d4e4
mementojunior123 <presumercj3@gmail.com>
2025-11-13 22:00:16 -0500

    Documented game_storage module

--- framework/core/base_game_storage.py
+++ framework/core/base_game_storage.py
@@ -8,36 +8,56 @@ if PLATFORM == 'emscripten':
     from platform import window
 
 class MockGameData(TypedDict):
+    """This is the base class for the game data that needs to be stored."""
     pass
 
 class BaseGameStorage:
-    """Most of these functions are incomplete and need implementing.\nThis module is made to handle file I/O and saving on multiple platforms."""
+    """This is the base class for game storage. It must be implemented in game_storage.py."""
     def __init__(self) -> None:
+        """Variables are initialised here."""
         pass
 
     def reset(self):
+        """Variables are set to their default values here."""
         pass
     
     def validate_data(self, data : MockGameData) -> bool:
+        """Function that validates that the game data that was passed to it is valid."""
         if data is None: return False
         return True
 
     def _get_data(self) -> MockGameData:
+        """Function that extracts the game data from the current storage object."""
         return {}
 
     def _load_data(self, data : MockGameData) -> bool:
+        """Function that loads the passed game data into the current storage object."""
         if not self.validate_data(data):
             print('Data is invalid!')
             return False
         return True
 
     def load(self, is_web : bool = False) -> bool:
+        """
+        Function that loads the current game data from file or from web.
+            is_web: True if we are currently in a web environnement, else False. 
+        Returns --> A boolean that represents the success status of the function.
+        """
         return self._load_from_file() if not is_web else self._load_from_web()
     
     def save(self, is_web : bool = False) -> None:
+        """
+        Function that saves the current game data to file or to web.
+            is_web: True if we are currently in a web environnement, else False.
+        """
         self._save_to_file() if not is_web else self._save_to_web()
 
     def _load_from_file(self, file_path : str = 'assets/data/game_info.json') -> bool:
+        """
+        Function that loads the current game data from a file.
+            file_path: The location of the game data file to load from. Defaults to "assets/data/game_info.json".
+        Returns --> A boolean that represents the success status of the function.
+        """
         with open(file_path, 'r') as file:
             data = json.load(file)
         if data:
@@ -45,11 +65,19 @@ class BaseGameStorage:
         return False
 
     def _save_to_file(self, file_path : str = 'assets/data/game_info.json') -> None:
+        """
+        Function that saves the current game data to file.
+            file_path: The location of the game data file to save to. Defaults to "assets/data/game_info.json".
+        """
         data = self._get_data()
         with open(file_path, 'w') as file:
             json.dump(data, file)
 
     def _load_from_web(self) -> bool:
+        """
+        Function that loads the current game data from web.
+        Returns --> A boolean that represents the success status of the function.
+        """
         web_data = self.get_web('GameData')
         if web_data is not None:
             data = json.loads(web_data)
@@ -58,11 +86,24 @@ class BaseGameStorage:
         return False
 
     def _save_to_web(self) -> None:
+        """
+        Function that saves the current game data to web.
+        """
         data = self._get_data()
         self.set_web('GameData', json.dumps(data))
 
-    def get_web(self, key : str) -> str:
+    def get_web(self, key : str) -> str|None:
+        """
+        Utility function that gets a value located in localStorage.
+            key: The name of the value to access.
+        Returns --> The string representing the value if it exists, otherwise None.
+        """
         return window.localStorage.getItem(key)
 
-    def set_web(self, key : str, value : Any):
+    def set_web(self, key : str, value : str):
+        """
+        Utility function that sets a value in localStorage.
+            key: The name of the value to set.
+            value: The value to set.
+        """
         window.localStorage.setItem(key, str(value))
--- src/game_storage.py
+++ src/game_storage.py
@@ -1,3 +1,5 @@
+"""This module is made to handle saving game data on multiple platforms."""
+
 from sys import platform as PLATFORM
 import json
 import os
@@ -9,26 +11,49 @@ if PLATFORM == 'emscripten':
     from platform import window
 
 class GameData(MockGameData):
+    """
+    A TypedDict that defines the data that is stored in the game storage.
+    """
     high_score : int
 
 class GameStorage(BaseGameStorage):
-    """Exemple implementation of BaseGameStorage."""
+    """
+    Implementation of BaseGameStorage.
+    """
     def __init__(self) -> None:
+        """Initialises all of the data."""
         self.high_score : int = 0
 
     def reset(self):
+        """
+        Resets the data to its default value.
+        """
         self.high_score = 0
     
     def validate_data(self, data : GameData) -> bool:
+        """
+        Function that verifies if data is valid game data.
+            data: The data to verifiy.
+        Returns --> True if the data is valid, else False.
+        """
         if data is None: return False
         if not isinstance(data.get("high_score", None), int):
             return False
         return True
 
     def _get_data(self) -> GameData:
+        """
+        Function that converts the game storage object to a dictonnary that contains the data.
+        Returns --> A GameData object (TypedDict).
+        """
         return {'high_score' : self.high_score}
 
     def _load_data(self, data : GameData) -> bool:
+        """
+        Function that loads game data into the storage object.
+            data: The data to load.
+        Returns --> True on success, else False.
+        """
         if not self.validate_data(data):
             print('Data is invalid!')
             return False
