1ddd61019ff050e9de2ac3dde41accc7a65204d5
mementojunior123 <presumercj3@gmail.com>
2026-01-17 19:23:23 -0500

    Finished the web audio API

--- framework/core/bg_manager.py
+++ framework/core/bg_manager.py
@@ -1,42 +1,164 @@
 import pygame
 import json
+import platform
+from typing import Union, TypeAlias
 
 class SoundTypes:
     music = 'Music'
     sfx = 'SFX'
 
 class WebChannel:
-    def __init__(self, path : str, sound_ref : pygame.mixer.Sound|None, channel_id : int, loop_count : int, volume : float,
-                  play_now : bool = True):
+    CHANNELS : dict[int, 'WebChannel'] = {}
+
+    @classmethod
+    def _get_unused_channel(cls, force : bool = False) -> int|None:
+        for i in range(core_object.bg_manager.MAX_CHANNEL_COUNT):
+            if i not in cls.CHANNELS:
+                return i
+        if force:
+            return 0
+        return None
+    
+    def __new__(cls, channel_id : int):
+        if channel_id in WebChannel.CHANNELS:
+            return WebChannel.CHANNELS[channel_id]
+        else:
+            new = super(WebChannel, cls).__new__(WebChannel)
+            WebChannel.CHANNELS[channel_id] = new
+
+        return new
+
+    def __init__(self, channel_id : int):
+        if hasattr(self, '_id'): return
+        self._id = channel_id
         self.bg_manager : BgManager = core_object.bg_manager
-        self.volume : float = volume
-        self._id : int = channel_id
+        self._volume : float = 0
+        self._sound_name : str|None = None
+        self._sound_ref : pygame.mixer.Sound|None = None
+        self._sound_path : str|None = None
+        self._actual_vol : float = -1
         core_object.run_js_source_file('make_web_channel', {
-            "AUDIO_PATH" : path,
-            "PLAY_NOW" : str(play_now).lower(),
-            "VOLUME" : str(sound_ref.get_volume() * self.bg_manager.global_volume * volume * self.bg_manager.web_mult),
+            "AUDIO_PATH" : 'assets/audio/NOTHING.ogg',
+            "PLAY_NOW" : "false",
+            "VOLUME" : "0",
             "CHANNEL_ID" : str(self._id),
-            "LOOP_COUNT" : str(loop_count),
+            "LOOP_COUNT" : "0",
         })
     
     @property
-    def id(self) -> int:
+    def id(self) -> int|None:
         return self._id
+
+    def play(self, sound_name : str, loops : int = 0, maxtime : float = 0, fade_ms : float = 0, volume : float = 1.0):
+        if self._id is None: return
+        if sound_name not in self.bg_manager.SOUNDS:
+            core_object.log(f"Sound {sound_name} not found!")
+            return
+        if fade_ms:
+            core_object.log(f"Warning : WebChannels do not support fade_ms")
+        if maxtime:
+            core_object.log(f"Warning : WebChannels do not support maxtime")
+        self._volume = volume
+        sound_obj, path = self.bg_manager.SOUNDS[sound_name]
+        self._sound_name = sound_name
+        self._sound_ref : pygame.mixer.Sound = sound_obj
+        self._sound_path : str = path
+        self._actual_vol : float = sound_obj.get_volume() * self._volume * self.bg_manager.web_mult
+        core_object.run_js_source_file('dispatch_event', {
+            "EVENT_TYPE" : "StartAudio",
+            "EVENT_ARGS" : json.dumps({'id' : self._id, 'path' : self._sound_path, 'volume' : self._actual_vol, 'target_loop_count' : loops})
+        })
     
     def pause(self):
+        if self._id is None: return
         core_object.run_js_source_file('dispatch_event', {
             "EVENT_TYPE" : "PauseAudio",
             "EVENT_ARGS" : json.dumps({'id' : self._id})
         })
     
+    def unpause(self):
+        if self._id is None: return
+        core_object.run_js_source_file('dispatch_event', {
+            "EVENT_TYPE" : "ResumeAudio",
+            "EVENT_ARGS" : json.dumps({'id' : self._id})
+        })
+    
+    def stop(self):
+        core_object.run_js_source_file('dispatch_event', {
+            "EVENT_TYPE" : "StopAudio",
+            "EVENT_ARGS" : json.dumps({'id' : self._id})
+        })
+        self._sound_ref = None
+        self._sound_path = None
+        self._sound_name = None
+
+    def get_volume(self) -> float:
+        return self._volume
+    
+    def set_volume(self, new_vol : float, arg2 : float|None=None):
+        if arg2 is not None:
+            core_object.log("Stereo sound not supported in the browser!")
+        if self._id is None: return
+        if new_vol != self._volume:
+            self._actual_vol : float = self._sound_ref.get_volume() * self._volume * self.bg_manager.web_mult
+            self._volume = new_vol
+            core_object.run_js_source_file('dispatch_event', {
+                "EVENT_TYPE" : "UpdateVolume",
+                "EVENT_ARGS" : json.dumps({'id' : self._id, 'volume' : self._actual_vol})
+            })
+
+    def _update_volume(self):
+        if self._id is None: return
+        new_vol : float = self._sound_ref.get_volume() * self._volume * self.bg_manager.web_mult
+        if new_vol != self._actual_vol:
+            self._actual_vol = new_vol
+            core_object.run_js_source_file('dispatch_event', {
+                "EVENT_TYPE" : "UpdateVolume",
+                "EVENT_ARGS" : json.dumps({'id' : self._id, 'volume' : self._actual_vol})
+            })
+    
+    def _update(self):
+        if self._id is None: return
+        self._update_volume()
+        core_object.run_js_source_file('dispatch_event', {
+            "EVENT_TYPE" : "UpdateAudioChannel",
+            "EVENT_ARGS" : json.dumps({'id' : self._id})
+        })
+        if not self.get_busy():
+            self._sound_ref = None
+            self._sound_path = None
+            self._sound_name = None
+        
+    
+    def get_sound(self) -> pygame.mixer.Sound|None:
+        return self._sound_ref
+    
+    def get_busy(self) -> bool:
+        if self._sound_ref is None: return False
+        k : str = 'WebAudioChannel' + str(self._id) + "_" + 'busy'
+        return (platform.window.localStorage.getItem(k) == "true")
+    
+    def queue(arg1):
+        core_object.log("Webchannel.queue is not implemented!")
+    
+    def get_queue() -> None:
+        core_object.log("Webchannel.get_queue is not implemented!")
+    
+    def set_endevent(t=None) -> None:
+        core_object.log("Webchannel.set_endevent is not implemented!")
+    
+    def get_endevent() -> None:
+        core_object.log("Webchannel.get_endevent is not implemented!")
+    
+AnyChannel : TypeAlias = Union[pygame.mixer.Channel, WebChannel]
 
 class BgManager:
-    WebChannel = WebChannel
+    MAX_CHANNEL_COUNT : int = pygame.mixer.get_num_channels()
     def __init__(self, core_refrence : "Core") -> None:
         global core_object
         core_object = core_refrence
         self.core : "Core" = core_refrence
-        self.current : dict[pygame.mixer.Channel, TrackInfo] = {}
+        self.current : dict[AnyChannel, TrackInfo] = {}
         self.global_volume = 1
         self.web_mult : float = 1.0
         self.sound_types = SoundTypes
@@ -51,20 +173,28 @@ class BgManager:
         self.SOUNDS : dict[str, tuple[pygame.mixer.Sound, str]] = {}
         sound_list : list[tuple[str, str, float]] = [
             ('test_music', 'assets/audio/music/test_music.ogg', 1.0),
-            ('test_sfx', 'assets/audio/sfx/test_sfx.ogg', 1.0)
+            ('test_sfx', 'assets/audio/sfx/test_sfx.ogg', 1.0),
+            ('PLACEHOLDER_DO_NOT_TOUCH', 'assets/audio/NOTHING.ogg', 1.0)
         ]
         for name, path, vol in sound_list:
             self.load_sound(path, vol, name)
-    
+
+    def find_unused_channel(force : bool = False) -> pygame.mixer.Channel|WebChannel|None:
+        WebChannel._get_unused_channel(force) if core_object.is_web() else pygame.mixer.find_channel(force)
+
     def load_sound(self, path : str, vol : float, name : str):
         sound = pygame.mixer.Sound(path)
         sound.set_volume(vol)
         self.SOUNDS[name] = (sound, path)
     
+    def get_sound_obj(self, sound_name : str) -> pygame.mixer.Sound|None:
+        return self.SOUNDS.get(sound_name, (None, None))[0]
+    
+    
     def test_play_web(self, name : str):
         if not self.core.is_web(): return
-        sound_ref, path = self.SOUNDS[name]
-        WebChannel(path, sound_ref, pygame.mixer.find_channel().id, -1, 0.1)
+        channel : WebChannel = WebChannel(WebChannel._get_unused_channel(True))
+        channel.play(name)
 
     def set_global_volume(self, new_volume):
         self.global_volume = new_volume
@@ -72,10 +202,20 @@ class BgManager:
             info : TrackInfo = self.current[channel]
             channel.set_volume(self.global_volume * info.volume)
 
-       
+    def _play_web(self, track_name : str, volume : float, loops = -1, maxtime = 0, fade_ms = 0, 
+                  sound_type : str|None = 'Music'):
+        channel : WebChannel = WebChannel(WebChannel._get_unused_channel(True))
+        channel.play(track_name, loops, maxtime, fade_ms, volume)
+        channel.set_volume(volume * self.global_volume)
+        self.current[channel] = TrackInfo(volume, sound_type)
 
-    def play(self, track : pygame.mixer.Sound, volume, loops = -1, maxtime = 0, fade_ms = 0, sound_type : str|None = 'Music'):
+    def play(self, track_name : str, volume, loops = -1, maxtime = 0, fade_ms = 0, sound_type : str|None = 'Music'):
         """Used for playing music."""
+        if core_object.is_web():
+            self._play_web(track_name, volume, loops, maxtime, fade_ms, sound_type)
+            return
+        else:
+            track = self.get_sound_obj(track_name)
         channel = track.play(loops, maxtime, fade_ms)
         if not channel:
             core_object.log("Attempted to play track, but ran out of audio channels!")
@@ -86,8 +226,13 @@ class BgManager:
         self.current[channel] = TrackInfo(volume, sound_type)
         return channel
     
-    def play_sfx(self, sfx : pygame.mixer.Sound, volume, loops = 0, maxtime = 0, fade_ms = 0, sound_type : str|None = 'SFX'):
+    def play_sfx(self, sfx_name : str, volume, loops = 0, maxtime = 0, fade_ms = 0, sound_type : str|None = 'SFX'):
         """Used for playing short sound effects."""
+        if core_object.is_web():
+            self._play_web(sfx_name, volume, loops, maxtime, fade_ms, sound_type)
+            return
+        else:
+            sfx = self.get_sound_obj(sfx_name)
         if not channel:
             core_object.log("Attempted to play sfx, but ran out of audio channels!")
             return
@@ -106,7 +251,7 @@ class BgManager:
     
     def get_all_type(self, t : str) -> list[pygame.mixer.Channel]:
         """Get all channels that are playing a sound of a specific type."""
-        channels : list[pygame.mixer.Channel] = []
+        channels : list[AnyChannel] = []
         for channel in self.current:
             info = self.current[channel]
             if info.type == t:
@@ -129,12 +274,13 @@ class BgManager:
         
         for channel in to_remove:
             self.current.pop(channel)
+            channel.stop()
         
         sound.stop()
     
     def stop_all_type(self, t : str):
         """Stop all sounds of a specific type."""
-        to_remove : list[pygame.mixer.Channel] = []
+        to_remove : list[AnyChannel] = []
         for channel in self.current:
             info = self.current[channel]
             if info.type == t:
@@ -156,8 +302,10 @@ class BgManager:
             
 
     def update(self):
-        to_remove : list[pygame.mixer.Channel] = []
+        to_remove : list[AnyChannel] = []
         for channel in self.current:
+            if isinstance(channel, WebChannel):
+                channel.update()
             if not channel.get_busy():
                 to_remove.append(channel)
         for channel in to_remove:
